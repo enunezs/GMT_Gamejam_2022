e86d1cba5e2214627980f62bda21d36a232c793b
Checkarn <69473759+Checkarn@users.noreply.github.com>
2022-07-16 19:55:25 +0100

    Added Menu
    Still needs work

---
+++ Lucky Pinball/Menu.gd
@@ -0,0 +1,14 @@
+extends Control
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$VBoxContainer/StartButton.grab_focus()
+	
+
+
+func _on_StartButton_pressed():
+	get_tree().change_scene("res://Table/Table.tscn")
+
+
+func _on_Quitbutton_pressed():
+	get_tree().quit()
